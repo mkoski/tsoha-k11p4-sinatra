6faac5500b21b31f72955484b1005c096853696f
Markus Koski <markus.koski@cs.helsinki.fi>
2011-04-17 10:17:16 +0300

    Fixing Datamapper problem

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "http://rubygems.org"
 gem "sinatra"
 gem "data_mapper"
 
+
 group :production do
   gem "dm-postgres-adapter"
 end
--- tsoha.rb
+++ tsoha.rb
@@ -6,29 +6,44 @@ require 'config/init'
 require 'models/user'
 require 'models/tables'
 
-#DataMapper.setup(:default, "sqlite3:tsoha.db")
 DataMapper.finalize
-DataMapper.auto_upgrade!
-
+DataMapper.auto_migrate!
 
 class Tsoha < Sinatra::Base
 
-  enable :sessions
   set :public, File.dirname(__FILE__) + "/public"
-
   $h2_header = "C D _ A R C H I V E<br><hr>"
-  
+
+# Navigations
+
   get '/' do
-#    @esimerkkimuuttuja = "tämä on muuttuja"
-#    @sessiosta_muuttujaan = session[:muuttuja]
-#    @testmodelin_arvot = User.all
     erb :index
   end
 
-  get '/add' do   
+  get '/add' do
     erb :add
   end
-  post '/add/addcd' do   
+
+  get '/error' do
+    erb :error
+  end
+
+  get '/added' do
+    erb :added
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+# Actions
+
+  post '/list' do
+    @compact_discs = CompactDisc.all
+    erb :listed
+  end
+
+  post '/add/addcd' do
     compactdisc = CompactDisc.new(params)
     if compactdisc.save
       redirect '/added'
@@ -36,28 +51,10 @@ class Tsoha < Sinatra::Base
       erb :error
     end
   end
-  post '/search' do  
+
+  post '/search' do
     @results = CompactDisc.get(params)
     erb :search
   end
-  get '/error' do 
-    erb :error
-  end
-  get '/added' do
-    erb :added
-  end
-  get '/login' do
-  
-    erb :login
-  end
-  get '/sessioon/:arvo' do
-    session[:muuttuja] = params[:arvo]
-    redirect '/'
-  end
-  post '/list' do
-    @compact_discs = CompactDisc.all
-    @results = @compact_discs.each {|cd| puts "#{cd.title} - #{cd.artist}"}
-    erb :listed
-  end
 
-  end
+end
