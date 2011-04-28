f546e94d0619ae339d260ac5dabb65ac85af0b0f
Markus Koski <markus.koski@cs.helsinki.fi>
2011-04-28 14:19:55 +0300

    test

--- models/user.rb
+++ models/user.rb
@@ -1,6 +1,15 @@
+require 'rubygems'
+require 'dm-core'
+require 'dm-migrations'
+require 'dm-validations'
+
 class User
   include DataMapper::Resource
   property :id, Serial
-  property :username, String, :required => true
+  property :username, String, :required => true, :unique => true,
+    :message => "Username already exists!"
   property :password, String, :required => true
 end
+
+DataMapper.finalize
+DataMapper.auto_migrate!
